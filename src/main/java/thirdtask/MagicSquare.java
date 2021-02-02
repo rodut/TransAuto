f2471b8a2168f593f238f8f7364dfb3d8015386a
Tudor <tudorache@gmail.com>
2021-02-02 10:07:03 +0200

    Third task.

---
+++ src/main/java/thirdtask/MagicSquare.java
@@ -0,0 +1,42 @@
+package thirdtask;
+
+import utils.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MagicSquare {
+    private static final String INPUTFILE = "src/main/resources/input_files/input_thirdtask";
+    private static final String OUTPUTFILE = "target/logs/output_thirdtask.txt";
+
+    public static void main(String[] args) throws IOException {
+        int[][] square = InputOutputUtils.readTwoDimensionalArrayFromFile(INPUTFILE);
+        checkIfMagicSquare(square);
+    }
+
+    public static void checkIfMagicSquare(int[][] array) throws IOException {
+        List<Integer> rowList = new ArrayList<Integer>();
+        List<Integer> columnList = new ArrayList<Integer>();
+        int size = array.length;
+        int sumOfFirstDiagonal = 0;
+        int sumOfSecondDiagonal = 0;
+        for (int i = 0; i < size; i++) {
+            sumOfFirstDiagonal += array[i][i];
+            sumOfSecondDiagonal += array[i][size - i - 1];
+            int sumEachRow = 0;
+            int sumEachCol = 0;
+            for (int j = 0; j < size; j++) {
+                sumEachRow += array[i][j];
+                sumEachCol += array[j][i];
+            }
+            rowList.add(sumEachRow);
+            columnList.add(sumEachCol);
+        }
+        if ((rowList.equals(columnList)) && (sumOfFirstDiagonal == sumOfSecondDiagonal)) {
+            InputOutputUtils.writeTwoDimensionalArrayToFile(array, OUTPUTFILE, "This is a magic square: \n");
+        } else {
+            InputOutputUtils.writeTwoDimensionalArrayToFile(array, OUTPUTFILE, "This is NOT a magic square: \n");
+        }
+    }
+}
--- src/main/java/utils/InputOutputUtils.java
+++ src/main/java/utils/InputOutputUtils.java
@@ -1,9 +1,6 @@
 package utils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class InputOutputUtils {
@@ -36,5 +33,39 @@ public class InputOutputUtils {
         File file = new File(path);
         Scanner scanner = new Scanner(file);
         return scanner.nextInt();
+    }
+
+    public static int[][] readTwoDimensionalArrayFromFile(String path) throws FileNotFoundException {
+        int[][] array;
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        int arraySize = scanner.nextLine().trim().split("\\s+").length;
+        scanner.close();
+        array = new int[arraySize][arraySize];
+        scanner = new Scanner(file);
+        int lineCount = 0;
+        while (scanner.hasNextLine()) {
+            String[] currentLine = scanner.nextLine().trim().split("\\s+");
+            for (int i = 0; i < currentLine.length; i++) {
+                array[lineCount][i] = Integer.parseInt(currentLine[i]);
+            }
+            lineCount++;
+        }
+        return array;
+    }
+
+    public static void writeTwoDimensionalArrayToFile(int[][] array, String path, String message) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                builder.append(array[i][j]);
+                if (j < array.length - 1)
+                    builder.append(" ");
+            }
+            builder.append("\n");
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        writer.write(message + builder.toString());
+        writer.close();
     }
 }
