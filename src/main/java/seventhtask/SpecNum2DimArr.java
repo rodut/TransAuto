4da5a94bc9f12c1fe38712867fbb8938a594d89d
Tudor <tudorache@gmail.com>
2021-02-19 09:17:09 +0200

    Seventh task: Find the specified number in a 2D array.

---
+++ src/main/java/seventhtask/SpecNum2DimArr.java
@@ -0,0 +1,31 @@
+package seventhtask;
+
+import utils.InputOutputUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpecNum2DimArr {
+    private static final String INPUT_FILE = "src/main/resources/input_files/input_seventhtask.txt";
+    private static final String OUTPUT_FILE = "target/logs/output_seventhtask.txt";
+
+    public static void main(String[] args) throws IOException {
+        int[][] arr = InputOutputUtils.readTwoDimensionalArrayFromFile(INPUT_FILE);
+        int n = 5;
+        InputOutputUtils.writeToFile(findSpecifiedNumber(arr, n), OUTPUT_FILE);
+    }
+
+    private static List<String> findSpecifiedNumber(int[][] arr, int num) {
+        List<String> locations = new ArrayList<>();
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr.length; col++) {
+                if (arr[row][col] == num) {
+                    String location = String.format("[%s, %s]", row, col);
+                    locations.add(location);
+                }
+            }
+        }
+        return locations;
+    }
+}
---
+++ src/main/java/seventhtask/Test.java
@@ -0,0 +1,31 @@
+package seventhtask;
+
+import utils.InputOutputUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    private static final String INPUT_FILE = "src/main/resources/input_files/test";
+    private static final String OUTPUT_FILE = "target/logs/text.txt";
+
+    public static void main(String[] args) throws IOException {
+        int[][] arr = InputOutputUtils.readTwoDimensionalArrayFromFile(INPUT_FILE);
+        InputOutputUtils.writeToFile(calcMaxValCol(arr), OUTPUT_FILE);
+    }
+
+    private static List<Integer> calcMaxValCol(int[][] arr) {
+        List<Integer> results = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            int a = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i][j] > a) {
+                    a = arr[i][j];
+                }
+            }
+            results.add(a);
+        }
+        return results;
+    }
+}
